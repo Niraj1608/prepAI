660cf79a4698f9befe90ddf5007d18b226140884
Niraj Parmar <nirajprmr1608@gmail.com>
2024-06-17 02:39:32 +0530

    Initial Commit

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,10 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import {
+  ClerkProvider,
+  
+} from '@clerk/nextjs'
+import { Toaster } from "@/components/ui/sonner";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -10,8 +15,11 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
+    <ClerkProvider>
     <html lang="en">
       <body className={inter.className}>{children}</body>
+<Toaster/>
     </html>
+    </ClerkProvider>
   );
 }
