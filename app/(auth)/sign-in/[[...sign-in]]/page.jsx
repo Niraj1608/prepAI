669cc44badaa6384c98935c24fe16e2a70ead7e3
Niraj Parmar <107384357+Niraj1608@users.noreply.github.com>
2024-06-17 18:03:48 +0530

    Update page.jsx

--- app/(auth)/sign-in/[[...sign-in]]/page.jsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.jsx
@@ -66,12 +66,11 @@ export default function Page() {
             </a>
   
             <h1 className="mt-2 text-2xl font-bold text-gray-900 sm:text-3xl md:text-4xl">
-              Welcome to AI Interview Mocker
+              Welcome to prepAI
             </h1>
   
             <p className="mt-4 leading-relaxed text-gray-500">
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eligendi nam dolorum aliquam,
-              quibusdam aperiam voluptatum.
+             Sign in to access your account.
             </p>
           </div>
           <SignIn />
